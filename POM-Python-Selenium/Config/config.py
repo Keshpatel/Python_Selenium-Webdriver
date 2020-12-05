139e7e984de69ce25f08bc676b79de3023b0e1f5
Keshini <kdpatel8184@gmail.com>
2020-12-05 00:04:26 -0700

    This is my first commit

---
+++ POM-Python-Selenium/Config/config.py
@@ -0,0 +1,9 @@
+class TestData:
+    CHROME_EXECUTABLE_PATH = "C:\\Users\\Keshini\\PycharmProjects\\POM-Python-Selenium\\Drivers\\chromedriver.exe"
+    FIREFOX_EXECUTABLE_PATH = "C:\\Users\\Keshini\\PycharmProjects\\POM-Python-Selenium\\Drivers\\geckodriver.exe"
+
+    BASE_URL = "https://classic.crmpro.com/"
+    USER_NAME = "kdpatel8184@gmail.com"
+    PASSWORD = "Krupa2011"
+
+    LOGIN_PAGE_TITLE = "CRMPRO  - CRM software for customer relationship management, sales, and support."
